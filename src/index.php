70a36cdb1dc6e077ea855d94d671432789b8ec99
mahieurobindev <robinmahieulp@gmail.com>
2022-01-10 15:50:34 +0100

    changement de la date

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 2 déc. 2021 à 16h31 </p>';
-phpinfo();
+echo '<p>update : 10 jan. 2022 à 15h49 </p>';
 ?>
 
