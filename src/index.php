e25dc6fcdc442bd6b9b2afd1a7912d5dda5c6a1e
mahieurobindev <robinmahieulp@gmail.com>
2022-01-10 16:09:36 +0100

    mise a jour date

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 10 jan. 2022 à 15h49 </p>';
+echo '<p>update : 10 jan. 2022 à 16h09 </p>';
 ?>
 
